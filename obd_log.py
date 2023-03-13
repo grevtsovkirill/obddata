32b3bf1c7d30df32106e37d89c00e0d03593889b
grevtsovkirill <grevtsovkirill@gmail.com>
2023-03-13 22:23:27 +0000

    add func

--- obd_log.py
+++ obd_log.py
@@ -16,3 +16,42 @@ print(response.value) # returns unit-bearing values thanks to Pint
 cmd_dtc = obd.commands.DTC_RPM
 response_dtc = connection.query(cmd_dtc)
 print("  dtc ", response_dtc)
+
+
+
+# OBD Globals
+obdRunning = True
+obdAvailable = False
+obdVoltage = 0
+obdCoolant = 0
+obdOil = 0
+obdLoad = 0
+obdIntake = 0
+
+# OBD Thread
+def obd_thread():
+    global obdRunning, obdAvailable, obdVoltage, obdCoolant, obdOil, obdLoad, obdIntake
+    obdConnection = obd.OBD()
+    print("OBD connected")
+    while obdRunning:
+        cmd = obd.commands.COOLANT_TEMP
+        response = obdConnection.query(cmd)
+        obdCoolant = response.value.magnitude
+        
+        cmd = obd.commands.ELM_VOLTAGE
+        response = obdConnection.query(cmd)
+        obdVoltage = response.value.magnitude
+        
+        #cmd = obd.commands.OIL_TEMP
+        #response = obdConnection.query(cmd)
+        #obdOil = response.value
+        
+        cmd = obd.commands.ENGINE_LOAD
+        response = obdConnection.query(cmd)
+        obdLoad = response.value.magnitude
+        
+        cmd = obd.commands.INTAKE_TEMP
+        response = obdConnection.query(cmd)
+        obdIntake = response.value.magnitude
+        obdAvailable = True
+        time.sleep(0.5)
